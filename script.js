e196c1925ce29181cd813ca61c1accd87cb30cef
juice2227 <myceline2227@gmail.com>
2024-07-04 04:07:45 +0300

    changes

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document loaded.');
 });
+
+
+function updateDateTime() {
+    const currentDate = new Date();
+    
+    
+    const currentTimeUTCElement = document.querySelector('[data-testid="currentTimeUTC"]');
+    if (currentTimeUTCElement) {
+        currentTimeUTCElement.textContent = currentDate.toISOString(); 
+    }
+    
+    
+    const currentDayElement = document.querySelector('[data-testid="currentDay"]');
+    if (currentDayElement) {
+        const options = { weekday: 'long' };
+        currentDayElement.textContent = currentDate.toLocaleDateString('en-US', options); 
+    }
+}
+
+
+updateDateTime();
+
+
+setInterval(updateDateTime, 1000);
